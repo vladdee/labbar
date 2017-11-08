1f1957f84ecf3de32aef56feeb370dd4f9e786e8
Vladimir Bondarenko <vladdeboika@gmail.com>
2017-11-08 15:15:01 +0100

    Assignment branch completed

--- network.py
+++ network.py
@@ -1,42 +1,44 @@
 from calc import *
 
-def poop(p, dic):
-    if not p:
-        return dic
-    elif isassignment(p):
-        dic[eval_expr(first_statement(p)), poop(rest_statements(p))
-        
-            
-
 ############################## Assignment functions ######################
-def assigner(assg_stmnt):
-    
-
-def eval_expr(expr):
+def eval_expr(expr, dic):
+    """
+    Function recieves an expression, checks if it's a binexpr, if so,
+    calls binary_calc and returns it's value, if the value's already been
+    assigned it just takes it from there instead of treating it as just a
+    string.
+    """
     if isbinary(expr):
-        return binary_calc(expr)
-    elif isconstant(expr) or isvariable(expr):
+        return binary_calc(expr, dic)
+    elif isconstant(expr):
         return expr
+    elif isvariable(expr):
+        if expr in dic:
+            return dic[expr]
+        else:
+            return expr
 
-def binary_calc(binexpr):
+def binary_calc(binexpr, dic):
+    """
+    Function recieves a binexpr and first evaluates the operator then calls
+    on it self semi-recursively via the eval_expr function. It finally returns
+    the value of the binary expression.
+    """
     if binary_operator(binexpr) == '+':
-        return eval_expr(binary_left(binexpr)) + eval_expr(binary_right(binexpr))
+        return eval_expr(binary_left(binexpr), dic) + eval_expr(binary_right(binexpr), dic)
     elif binary_operator(binexpr) == '-':
-        return eval_expr(binary_left(binexpr)) - eval_expr(binary_right(binexpr))
+        return eval_expr(binary_left(binexpr), dic) - eval_expr(binary_right(binexpr), dic)
     elif binary_operator(binexpr) == '/':
-        return eval_expr(binary_left(binexpr)) / eval_expr(binary_right(binexpr))
+        return eval_expr(binary_left(binexpr), dic) / eval_expr(binary_right(binexpr), dic)
     elif binary_operator(binexpr) == '*':
-        return eval_expr(binary_left(binexpr)) * eval_expr(binary_right(binexpr))
-    
+        return eval_expr(binary_left(binexpr), dic) * eval_expr(binary_right(binexpr), dic)
+
+ # fråga carl om bättre sätt att föra vidare dictionaryn
 ###########################
 
 def eval_program(calc_prog, optional = {}):
-    if isprogram(calc_prog): #check if calc prog is a valid prog
-        if isstatements(program_statements(calc_prog)):
-            if isassignment(first_statement(program_statements(calc_prog))):
-                optional[first_statement(program_statements(calc_prog))[1]] = eval_expr(first_statement(program_statements(calc_prog))[2])
-                return optional
-                                                               
-                    
-            
-            
+    if isprogram(calc_prog) and isstatements(program_statements(calc_prog)): #check if calc prog is a valid prog
+        for stmnt in program_statements(calc_prog):
+            if isassignment(stmnt):
+                optional[stmnt[1]] = eval_expr(stmnt[2], optional)
+    return optional
